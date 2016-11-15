b62b3540649cabd50f6bc094dd22142730712667
Matvei Nazaruk <matvei.nazaruk@gmail.com>
2016-11-15 18:43:38 +0300

    Added system calls errors handling.

--- pipes/main.cpp
+++ pipes/main.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <sstream>
+#include <iterator>
 
 char ** toProcessArgs(std::vector<std::string> &args) {
     auto processArgs = new char *[args.size() + 1];
@@ -23,8 +25,12 @@ int main(int argc, char ** argv) {
     args.push_back(std::vector<std::string>());
     for (int i = 1; i < argc; ++i) {
         if (strcmp(argv[i], "|") == 0) {
-            args.push_back(std::vector<std::string>());
+            if (args[childrenCount - 1].size() == 0) {
+                std::cerr << "ARGUMENTS ERROR: There sould be at least 1 command beetween pipes" << std::endl;
+                return 2;
+            }
             childrenCount++;
+            args.push_back(std::vector<std::string>());
         } else {
             std::string arg (argv[i]);
             args[childrenCount - 1].push_back(arg);
@@ -34,10 +40,11 @@ int main(int argc, char ** argv) {
     int pipefds[childrenCount - 1][2];
     for (int i = 0; i < childrenCount - 1; ++i) {
         if (pipe(pipefds[i]) == -1) {
-            perror("pipe");
+            perror("Pipe creating error");
             return 1;
         }
     }
+
     pid_t * children = new pid_t [childrenCount];
 
     for (int i = 0; i < childrenCount; ++i) {
@@ -45,27 +52,45 @@ int main(int argc, char ** argv) {
 
         if (pid == 0) {
             if (i != 0) {
-                dup2(pipefds[i - 1][0], 0);
-                close(pipefds[i - 1][1]);
+                if (dup2(pipefds[i - 1][0], 0) == -1) {
+                    perror("Duplicate standard input error");
+                }
+                if (close(pipefds[i - 1][1]) == -1) {
+                    perror("Close pipe file descriptor for writing error.");
+                }
             }
             if (i != childrenCount - 1) {
-                dup2(pipefds[i][1], 1);
-                close(pipefds[i][0]);
+                if (dup2(pipefds[i][1], 1) == -1) {
+                    perror("Duplicate standard output error");
+                }
+                if (close(pipefds[i][0]) == -1) {
+                    perror("Close pipe file descriptor for reading error.");
+                }
             }
 
-            for (int j = 0; j < childrenCount - 1; ++j) {
-                if (j != i - 1 && j != i) {
-                    close(pipefds[j][0]);
-                    close(pipefds[j][1]);
+            for (int j = i + 1; j < childrenCount - 1; ++j) {
+                if (close(pipefds[j][0]) == -1) {
+                    perror("Close pipe file descriptor for reading error.");
+                }
+                if (close(pipefds[j][1]) == -1) {
+                    perror("Close pipe file descriptor for writing error.");
                 }
             }
 
             auto processArgs = toProcessArgs(args[i]);
-            execvp(args[i][0].c_str(), processArgs);
+            if (execvp(args[i][0].c_str(), processArgs) == -1) {
+                std::cerr << "Error executing '";
+                std::for_each(args[i].begin(), args[i].end(), [](std::string &arg) { std::cerr << arg << " ";});
+                std::cerr << "'\n";
+                perror("");
+                break;
+            }
         }
 
         if (i != 0) {
-            close(pipefds[i - 1][1]);
+            if (close(pipefds[i - 1][1]) == -1) {
+                perror("Close pipe file descriptor for writing error.");
+            }
         }
 
         children[i] = pid;
@@ -86,5 +111,6 @@ int main(int argc, char ** argv) {
         close(pipefds[i][0]);
     }
     delete children;
+
     return exitCode;
 }
