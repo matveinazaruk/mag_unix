0e5254abba168fad31f1845b676a304762ca24ad
matveinazaruk <matvei.nazaruk@gmail.com>
2016-12-12 19:41:03 +0300

    Fixed escaping os backslash

--- shquote/shquote.sh
+++ shquote/shquote.sh
@@ -26,6 +26,6 @@ if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
 fi
 
 read command
-params="$(sed 's_\("\)_\\\1_g' | sed 's/\(.*\)/"\1"/g' | awk 'BEGIN{FS="\n"; ORS="\0";}{print}')"
+params="$(sed 's_\([\\"]\)_\\\1_g' | sed 's/\(.*\)/"\1"/g' | awk 'BEGIN{FS="\n"; ORS="\0";}{print}')"
 #params="$(sed 's_\("\)_\\\1_g' | sed 's/\(.*\)/"\1"/g' | awk '{printf "%s%s", sep,$1; sep="|"}' FS="\n")"
 echo "$command $params"
